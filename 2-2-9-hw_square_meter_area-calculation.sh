f645ea0600fd98495cdd2f0028226f3e855a39d1
mikeleegit <l0916737305@gmail.com>
2020-06-04 15:50:29 +0800

    hw

--- 2-2-9-hw_square_meter_area-calculation.sh
+++ 2-2-9-hw_square_meter_area-calculation.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "hello $1 $2"
+#echo "hello $1 $2"
+
+case $1 in 
+	square_meter)
+		square_meter_to_ping=`echo "scale=4;$2 * 0.3025"|bc`
+		echo "square_meter convert ping is "$square_meter_to_ping""
+		;;
+	ping)
+		ping_to_square_meter=`echo "scale=4;$2  / 0.3025"|bc`
+		echo "ping convert square_meter is "$ping_to_square_meter""	
+		;;
+esac
--- 3-2-1-hw_changeNetwork_base.sh
+++ 3-2-1-hw_changeNetwork_base.sh
@@ -3,7 +3,7 @@
 #Write by  350  (weilin.jang@gmail.com)
 #Version:  v1.00
 
-#[  $USER  !=  "root"  ]  &&  echo  "You  are  not  root,  exit!"  &&  exit  99
+[  $USER  !=  "root"  ]  &&  echo  "You  are  not  root,  exit!"  &&  exit  99
 
 #Netwrok  SETUP  0  (for  default)
 setLocal[0]="HOME"
@@ -18,17 +18,14 @@ setNetmask[1]="255.255.255.0"
 setGateway[1]="192.168.8.254"
 
 #Netwrok  SETUP  2
-setLocal[2]="III"
-setIP[2]="10.120.3.50"
-setNetmask[2]="255.255.0.0"
-setGateway[2]="10.120.0.1"
+setLocal[2]="WIFI"
+#setSsid[2]="iii"
+#setPasswd[2]="034257387"
+#setIP[2]="10.120.3.50"
+#setNetmask[2]="255.255.0.0"
+#setGateway[2]="10.120.0.1"
 
 
-#Netwrok  SETUP  3
-setLocal[3]="WIFI"
-#setIP[3]="192.168.196.119"
-#setNetmask[3]="255.255.255.0"
-#setGateway[3]="192.168.199.254"
 
 echo  -e  "===  FAST  CHANGE  NETWORK  ===\n  You  want  change  to:\n"
 x=0
@@ -46,15 +43,16 @@ case  ${varSelect:-"0"} in
                 route  del  default  &>  /dev/null
                 route  add  default  gw  ${setGateway[$varSelect]}
                 ;;
-	3)
-		#iwlist wlx503eaa6b0165  scanning |grep ESSID
-		#[ ESSID != iii ] && exit 0
-	      	 iwconfig wlx503eaa6b0165  essid III &> /dev/null
-	      	 iwconfig wlx503eaa6b0165  key 034257387 &> /dev/null
-			 dhclient wlx503eaa6b0165
-                #route  del  default  &>  /dev/null
-                #route  add  default  gw  ${setGateway[$varSelect]}
-		;;
+		 3)
+              #iwconfig wlx503eaa6b0165  essid III
+              #iwconfig wlx503eaa6b0165  key 034257387
+         	  #dhclient wlx503eaa6b0165
+              
+			  iwconfig wlan0  essid III
+              iwconfig wlan0  key 034257387
+         	  dhclient wlan0
+          		;;
+
         * )
                 echo  "No  \"$varSelect\",  please  check  again."  ;  exit  99
                 ;;
@@ -62,5 +60,4 @@ esac
 echo  "Successfully  set  ${setLocal[$varSelect]}  network."
 
 exit  0
-
 
